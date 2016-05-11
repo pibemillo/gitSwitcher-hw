5337f32c64c1e697e5dc47f860abd8e28a5e23b5
Gonzalo Aragon <gonzalo@ranklab.com>
2016-05-10 18:30:59 -0700

    hw

---
+++ gitSwitcher.js
@@ -0,0 +1,31 @@
+var day = 'Monday'
+
+switch(day) {
+	case 'Monday' :
+		console.log('I like pears');
+		break;
+	case 'Tuesday':
+		console.log('I like apples');
+		break;	
+	case 'Wednesday':
+		console.log('I like apricots');
+		break;	
+	case 'Thursday':
+		console.log('I like bananas');
+		break;	
+	case 'Friday':
+		console.log('I like oranges');
+		break;	
+	case 'Saturday':
+		console.log('I like everything');
+		break;
+	case 'Sunday':
+		console.log('Sleeping');
+		break;							
+	default:
+		console.log('What day is it?')	
+}
+
+
+
+
